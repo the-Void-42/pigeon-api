c5b7241ed009c196a35c4c5cf26a7ca22f51e1d2
sarnickaa <sarnicka.aleks@gmail.com>
2018-10-11 12:01:51 -0400

    added comments

--- server.js
+++ server.js
@@ -7,8 +7,14 @@ const passport = require('passport')
 
 const app = express()
 
+// bodyParser parses incoming requests before they hit routes/handlers. Allows access to req.body
 app.use(bodyParser.urlencoded({extended: false}))
+// a shallow alogorithm is used to parse incoming requests - will create a req.body consisting of key/value pairs where the value can only be a string or array
+// if extended: true - value can be of any type i.e deeply nested object
+
 app.use(bodyParser.json())
+// will only parse json data
+
 
 const db = require('./config/keys.js').mongoURI
 
@@ -17,9 +23,14 @@ mongoose.connect(db)
 .then(() => console.log('MongoDB Connected'))
 .catch(err => console.log(err))
 
+// test route:
 app.get('/', (req, res) => res.send('hi'))
 
 app.use(passport.initialize())
+//initialize the passport authentication module
+//allows it access to incoming requests
+
+//TODO: set up authentication strategy
 
 const port = process.env.PORT || 5000
 
